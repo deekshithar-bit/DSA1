3ba083e3a354b6aa126205a9f205bd71fba64029
Deekshitha R <deekshithar3618@gmail.com>
2026-08-13 22:57:13 +0530

    Add files via upload

--- 135.py
+++ 135.py
@@ -1,24 +1,58 @@
-T = int(input()) #Dutch National Flag algorithm
+def findBridges(V, edges): #Tarjan’s DFS algorithm
+    graph = [[] for _ in range(V)]
 
-for _ in range(T):
-    n = int(input())
-    A = list(map(int, input().split()))
+    # Build adjacency list
+    for u, v in edges:
+        graph[u].append(v)
+        graph[v].append(u)
 
-    low = 0
-    mid = 0
-    high = n - 1
+    disc = [-1] * V
+    low = [-1] * V
+    time = 0
+    bridges = []
 
-    while mid <= high:
-        if A[mid] == 0:
-            A[low], A[mid] = A[mid], A[low]
-            low += 1
-            mid += 1
+    def dfs(u, parent):
+        nonlocal time
 
-        elif A[mid] == 1:
-            mid += 1
+        disc[u] = low[u] = time
+        time += 1
 
-        elif A[mid] == 2:
-            A[mid], A[high] = A[high], A[mid]
-            high -= 1
+        for v in graph[u]:
 
-    print(*A)
+            # Ignore the edge back to parent
+            if v == parent:
+                continue
+
+            # If v is not visited, it is a DFS tree edge
+            if disc[v] == -1:
+                dfs(v, u)
+
+                # Update low value of u
+                low[u] = min(low[u], low[v])
+
+                # Bridge condition
+                if low[v] > disc[u]:
+                    bridges.append([u, v])
+
+            else:
+                # Back edge
+                low[u] = min(low[u], disc[v])
+
+    # Graph is connected, so starting from 0 is enough.
+    # This loop also makes the code work for disconnected graphs.
+    for u in range(V):
+        if disc[u] == -1:
+            dfs(u, -1)
+
+    return bridges 
+
+V = 5
+edges = [
+    [0, 1],
+    [1, 2],
+    [2, 0],
+    [1, 3],
+    [3, 4]
+]
+
+print(findBridges(V, edges))    
